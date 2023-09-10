475b8b86c520681df3b36458c7687c9b3153f724
abdi430 <abdibale9@gmail.com>
2023-09-10 02:52:23 -0700

    low level programming c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main-Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of int: %lu byte(s)\n" unsigned long(sizeof(i)));
+	printf("Size of char: %lu byte(s)\n" unsigned long(sizeof(c)));
+	printf("Size of long int: %lu byte(s)\n" unsigned long(sizeof(li)));
+	printf("Size of long long int: %lu byte(s)\n" unsigned long(sizeof(lli)));
+	printf("Size of float: %lu byte(s)\n" unsigned long(sizeof(f)));
