e3710010c9ccb62873d2e6db9bcebe4d523c0f99
abdi430 <abdibale9@gmail.com>
2023-09-16 04:18:40 -0700

    8 fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,9 @@ int print_last_digit(int n)
 	int l;
 
 	if (n < 0)
-		l = ( -1 * (n % 10));
+		l = (-1 * (n % 10));
 	else
 		l = n % 10;
-	_putchar(( l % 10) + '0');
+	_putchar((l % 10) + '0');
 	return (l % 10);
 }
