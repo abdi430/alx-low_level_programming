8f492630b9c16113d53e713c0bc8a0d202ed9caf
abdi430 <abdibale9@gmail.com>
2023-09-20 10:02:52 -0700

    0x5

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int interm;
+	interm = *a;
+	*b = *a;
+	*b = sw;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * _strlen - returns str length
+ * @s: pointer to a string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len =0;
+
+	while(s[len] != '\0')
+		len++;
+	return (len);
+}
