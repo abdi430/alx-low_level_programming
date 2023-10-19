6dc4c07b3fb4f478b170a22c8d91cfda3a799ceb
abdi430 <abdibale9@gmail.com>
2023-10-19 07:16:08 -0700

    singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+/*
+ * File: 1-listint_len.c
+ */
+
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - Returns the number of elements
+ *               in a linked list_t list.
+ * @h: A pointer to the head of the list_t list.
+ *
+ * Return: The number of elements in the list_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
