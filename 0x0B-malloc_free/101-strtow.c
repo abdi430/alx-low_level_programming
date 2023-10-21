5eb7617425b1b5962e4b3763f91fb886447868a6
abdi430 <abdibale9@gmail.com>
2023-10-21 04:28:23 -0700

    malloc free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,64 +1,91 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
+ * wstrlen - Calculates position of a word in a str
+ * @str: type char str
+ * description: function that returns a value position
+ * return: wstrlen 0
+ */
+int wstrlen(char *str)
+{
+	int pos = 0;
+	int len = 0;
+
+	for (pos = 0; (*(str + pos)) && *(str + pos) != ' '; pos++, len++)
+		;
+	return (len);
+}
+
+/**
+ * wcounter - Counter of words in s str
+ * @str: type char str
+ * description: function that returns a value string
+ * return: wcounter 0
+ */
+int wcounter(char *str)
+{
+	int pos = 0;
+	int len = 0;
+	int w = 0;
+
+	for (pos = 0; *(str + pos); pos++)
+		len++;
+	for (pos = 0; pos < len; pos++)
+	{
+		if (*(str + pos) != ' ')
+		{
+			w++;
+			pos = pos + wstrlen(str + pos);
+		}
+	}
+	return (w);
+}
+/**
+ * **strtow - Function that splits a string into words.
+ * @str: Type char str
+ * description: function
+ * Return: If str NULL or str = "" or function fails - NULL.
  */
 char **strtow(char *str)
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+	char **strs;
+	int pos = 0;
+	int letter, w, x, y;
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
-	if (*str == '\0' || str == NULL)
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
-	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == NULL)
+
+	w = wcounter(str);
+	if (w == 0)
 		return (NULL);
-	for (wordf = 0; str[wordf] && j <= w; wordf++)
+
+	strs = malloc(sizeof(char *) * (w + 1));
+	if (strs == NULL)
+		return (NULL);
+
+	for (x = 0; x < w; x++)
 	{
-		count = 0;
-		if (str[wordf] != ' ')
+		while (str[pos] == ' ')
+			pos++;
+
+		letter = wstrlen(str + pos);
+
+		strs[x] = malloc(sizeof(char) * (letter + 1));
+
+		if (strs[x] == NULL)
 		{
-			for (i = wordf ; str[i] != '\0'; i++)
-			{
-				if (str[i] == ' ')
-					break;
-				count++;
-			}
-			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + j) == NULL)
-			{
-				for (k = 0; k <= j; k++)
-				{
-					x = p[k];
-					free(x);
-				}
-				free(p);
-				return (NULL);
-			}
-			for (m = 0; wordf < i; wordf++)
-			{
-				p[j][m] = str[wordf];
-				m++;
-			}
-			p[j][m] = '\0';
-			j++;
+			for (; x >= 0; x--)
+				free(strs[x]);
+			free(strs);
+			return (NULL);
 		}
+		for (y = 0; y < letter; y++)
+			(*(*(strs + x) + y)) = str[pos++];
+		(*(*(strs + x) + y)) = '\0';
 	}
-	p[j] = NULL;
-	return (p);
+	strs[x] = NULL;
+
+	return (strs);
 }
