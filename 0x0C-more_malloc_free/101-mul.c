42a6c8d3ca30ffbfb39fc4cc859379a44b56f2c9
abdi430 <abdibale9@gmail.com>
2023-10-21 07:09:37 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,7 +29,7 @@ void _is_zero(char *argv[])
 
 	if (isn1 == 1 || isn2 == 1)
 	{
-		_putchar("0\n");
+		printf("0\n");
 		exit(0);
 	}
 }
@@ -66,7 +66,7 @@ int _checknum(char *argv[], int n)
 	for (ln = 0; argv[n][ln]; ln++)
 		if (!isdigit(argv[n][ln]))
 		{
-			_putchar("Error\n");
+			printf("Error\n");
 			exit(98);
 		}
 
@@ -87,11 +87,11 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-		_putchar("Error\n"), exit(98);
+		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
-		_putchar("Error\n"), exit(98);
+		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
@@ -121,7 +121,6 @@ int main(int argc, char *argv[])
 			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-	_putchar(nout);
-	_putchar('\n');
+	printf("%s\n", nout);
 	return (0);
 }
