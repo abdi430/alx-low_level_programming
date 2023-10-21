fe09b83153f7902146fc39e04ade839c7c9cc9f6
abdi430 <abdibale9@gmail.com>
2023-10-20 22:55:18 -0700

    strg conct

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,18 +3,49 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Entry point
- *@b: string we need to duplicate
- * Return: a pointer to the allocate memory
+ * string_nconcat - Entry point
+ *@s1: string 1
+ *@s2: string 2
+ *@n: number of bytes
+ * Return: pointer should point to a newly allocated space in memory or NULL
  */
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *p = NULL;
+	char *strnew = NULL;
+	unsigned int i, n1, n2, j, count, palabras;
 
-	p = malloc(b);
-	if (p == NULL)
+	count = 0;
+	palabras = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (n1 = 0; s1[n1] != '\0'; n1++)
+		;
+	for (n2 = 0; s2[n2] != '\0'; n2++)
+		;
+	if (n >= n2)
 	{
-		exit(98);
+		palabras = n2;
+
+	} else
+	{
+		for (n2 = 0; n2 < n; n2++)
+			palabras++;
 	}
-	return (p);
+	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+	if (strnew == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		strnew[i] = s1[i];
+	for (j = 0; j < palabras; i++)
+	{
+		strnew[i] = s2[count];
+		count++;
+		j++;
+	}
+	strnew[i] = '\0';
+	return (strnew);
 }
